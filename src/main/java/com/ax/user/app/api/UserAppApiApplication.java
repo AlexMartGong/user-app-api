6f15b3eb13e46c36f5975009ed8823b5a107aa7c
AlexMartGong <155708055+AlexMartGong@users.noreply.github.com>
2025-07-09 17:55:55 -0600

    Initialize Spring Boot project for `user-app-api` with Maven configuration, essential dependencies, and project structure.

---
+++ src/main/java/com/ax/user/app/api/UserAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.ax.user.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ax/user/app/api/UserAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ax.user.app.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
